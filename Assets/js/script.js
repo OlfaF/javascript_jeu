8595a1d09a37e70e71c9099c0027cd3b253cc633
Olfa <Olfa@email.com>
2025-10-15 12:26:22 +0200

    Debut exercice js

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+//  console.log("Du javascript !");
+
+const powers = ["feu", "eau", "terre"];
+// console.log(powers);
+function powerPc() {
+  const index = Math.floor(Math.random() * powers.length);
+  return powers[index];
+}
+
+
+
+
